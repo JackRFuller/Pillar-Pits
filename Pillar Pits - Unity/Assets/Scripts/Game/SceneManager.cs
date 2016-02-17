2cbff54303cf4dac3e586eb96772a9a41b4dc2fe
JackRFuller <jackrfuller@outlook.com>
2016-02-17 20:23:16 +0000

    Initial Scene Setup

---
+++ Pillar Pits - Unity/Assets/Scripts/Game/SceneManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        ResetScene();
+    }
+
+    void ResetScene()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+             Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
