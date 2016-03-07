b24bee8b4da7bd19c35b9b1c9916ce8894be5867
JackRFuller <jackrfuller@outlook.com>
2016-03-07 21:15:42 +0000

    Spline Walker
    - Added Spline Walker Script
    - Will probably re-do

---
+++ Pillar Pits - Unity/Assets/Editor/Moving Objects/LineInspector.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ObjectLine))]
+public class LineInspector : Editor
+{
+    private void OnSceneGUI()
+    {
+        ObjectLine _line = target as ObjectLine;
+        Transform handleTransform =_line.transform;
+        Quaternion handleRotation = Tools.pivotRotation == PivotRotation.Local ? 
+            handleTransform.rotation : Quaternion.identity;
+
+        Vector3 point0 = handleTransform.TransformPoint(_line.pointA);
+        Vector3 point1 = handleTransform.TransformPoint(_line.pointB);
+
+        Handles.color = Color.white;
+        Handles.DrawLine(point0, point1);
+        Handles.DoPositionHandle(point0, handleRotation);
+        Handles.DoPositionHandle(point1, handleRotation);
+
+        EditorGUI.BeginChangeCheck();
+        point0 = Handles.DoPositionHandle(point0, handleRotation);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(_line, "Move Point");
+            EditorUtility.SetDirty(_line);
+            _line.pointA = handleTransform.InverseTransformPoint(point0);
+        }
+            
+
+        EditorGUI.BeginChangeCheck();
+        point1 = Handles.DoPositionHandle(point1, handleRotation);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(_line, "Move Point");
+            EditorUtility.SetDirty(_line);
+            _line.pointB = handleTransform.InverseTransformPoint(point1);
+        }
+            
+
+
+    }
+	
+}
