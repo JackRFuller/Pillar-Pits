51059e7900d3cd00d1a0b5f93ed93dea3deeb087
JackRFuller <jackrfuller@outlook.com>
2016-03-08 19:33:45 +0000

    Lift
    Implemented lift w/custom inspector

---
+++ Pillar Pits - Unity/Assets/Editor/Gameplay Objects/Lift/LiftInspector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LiftBehaviour))]
+public class LiftInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        LiftBehaviour thisLift = (LiftBehaviour)target;
+
+        if (GUILayout.Button("Set Starting Position"))
+            thisLift.SetStartPos();
+
+        if (GUILayout.Button("Set End Position"))
+            thisLift.SetEndPos();
+
+        if (GUILayout.Button("Move To Start Position"))
+            thisLift.MoveToStartPosition();
+    }
+
+}
