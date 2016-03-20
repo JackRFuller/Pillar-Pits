67f504da80e4d34e5cc305a90b6d44acc49a7a6a
JackRFuller <jackrfuller@outlook.com>
2016-03-20 16:52:53 +0000

    Bullet Time Icon
    - Bullet Time now drains and is recharged
    - Two Modes: Once and Recharges

--- Pillar Pits - Unity/Assets/Scripts/Prototyping/BulletTime.cs
+++ Pillar Pits - Unity/Assets/Scripts/Prototyping/BulletTime.cs
@@ -3,38 +3,106 @@ using System.Collections;
 
 public class BulletTime : MonoBehaviour
 {
-    [SerializeField] float currentSlowMo = 0;
-    [SerializeField] float slowTimeAllowed = 2.0f;	
+    private enum bulletTimeModes
+    {
+        Once,
+        Recharge,
+    }
+
+    [SerializeField] private bulletTimeModes currentBulletTimeMode;
+
+    [Header("Slow Mo Editable Values")]   
+    [SerializeField] private float slowTimeAllowed = 2.0f;
+    [SerializeField] private float timeBeforeRechargeTime = 2.0f;    
+    private bool isRecharging;
+    private float currentSlowMo = 0;
+
+    void Start()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        currentSlowMo = slowTimeAllowed;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        //Start Bullet Time
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            if (Time.timeScale == 1.0f)
-                    Time.timeScale = 0.3f;
-            else
+            RunBulletTime();
+            StopCoroutine(StartRecharging());
+            isRecharging = false;              
+        }
+
+        //Stop Bullet Time
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            StopBulletTime();
+        }
+
+        //Check Bullet Time Hasn't Run Out
+        if(Time.timeScale == 0.3f)
+        {
+            currentSlowMo -= Time.deltaTime;
+
+            float _percentageTaken = (currentSlowMo / slowTimeAllowed) * 100.0f;
+            PlayerUIManager.instance.ChangeBulletTimeMeter(_percentageTaken);
+
+            if (currentSlowMo <= 0)
             {
-                Time.timeScale = 1.0f;
-                Time.fixedDeltaTime = 0.02f * Time.timeScale;
-            }            
+                StopBulletTime();
+            }
         }
 
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            currentSlowMo = 0;
-            Time.timeScale = 1.0f;
-        }     
-
-        if(Time.timeScale == 0.3)
-        {
-            currentSlowMo += Time.deltaTime;
-        }
-
-        if(currentSlowMo > slowTimeAllowed)
-        {
-            currentSlowMo = 0;
-            Time.timeScale = 1.0f;
-        }
+        //Recharge BulletTime
+        if (isRecharging)
+            RechargeBulletTime();
 	}
+
+    void RunBulletTime()
+    {        
+        if(Time.timeScale == 1.0f)
+        {
+            Time.timeScale = 0.3f;
+        }            
+        else
+        {
+            Time.timeScale = 1.0f;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;            
+        }  
+    }
+
+    void StopBulletTime()
+    {
+       
+        Time.timeScale = 1.0f;
+        //Start Bullet Time Recharge
+        if (currentBulletTimeMode == bulletTimeModes.Recharge)
+            StartCoroutine(StartRecharging());     
+    }
+
+    IEnumerator StartRecharging()
+    {
+        yield return new WaitForSeconds(timeBeforeRechargeTime);
+        isRecharging = true;
+    }
+
+    void RechargeBulletTime()
+    {
+        currentSlowMo += Time.deltaTime;
+        float _percentageTaken = (currentSlowMo / slowTimeAllowed) * 100.0f;
+        PlayerUIManager.instance.ChangeBulletTimeMeter(_percentageTaken);
+
+        if (currentSlowMo >= slowTimeAllowed)
+        {
+            currentSlowMo = slowTimeAllowed;
+            isRecharging = false;           
+        }
+    }
+
+
 }
---
+++ Pillar Pits - Unity/Assets/Scripts/UI/PlayerUIManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerUIManager : MonoBehaviour {
+
+    public static PlayerUIManager instance;
+
+    [Header("Bullet Time")]
+    [SerializeField] private Image bulletTimeMeter;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}	
+
+    public void ChangeBulletTimeMeter(float _meter)
+    {
+        float _fillAmount = _meter / 100.0f;
+        if(_fillAmount < 0)
+        {
+            _fillAmount = 0;
+        }
+        bulletTimeMeter.fillAmount = _fillAmount;
+    }
+}
