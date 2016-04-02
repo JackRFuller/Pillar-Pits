44634c699e8902d461910e8e112b21dfd73c6f58
JackRFuller <jackrfuller@outlook.com>
2016-04-02 11:55:55 +0100

    Re-done the Bullet Time Bar

--- Pillar Pits - Unity/Assets/Scripts/UI/PlayerUIManager.cs
+++ Pillar Pits - Unity/Assets/Scripts/UI/PlayerUIManager.cs
@@ -8,6 +8,7 @@ public class PlayerUIManager : MonoBehaviour {
 
     [Header("Bullet Time")]
     [SerializeField] private Image bulletTimeMeter;
+    [SerializeField] private float timeUntilTurnOff; //Wait Time Before Turning off the Meter
 
     void Awake()
     {
@@ -22,17 +23,42 @@ public class PlayerUIManager : MonoBehaviour {
     }
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        Init();
 	}	
+
+    void Init()
+    {
+        bulletTimeMeter.gameObject.SetActive(false);
+    }
 
     public void ChangeBulletTimeMeter(float _meter)
     {
+        if (!bulletTimeMeter.gameObject.activeInHierarchy)
+                bulletTimeMeter.gameObject.SetActive(true);
+
         float _fillAmount = _meter / 100.0f;
         if(_fillAmount < 0)
         {
             _fillAmount = 0;
         }
+
+        if(_fillAmount >= 1)
+        {
+            _fillAmount = 1;
+            StartCoroutine(WaitBeforeTurningOffBulletTime());
+        }
+
         bulletTimeMeter.fillAmount = _fillAmount;
+
     }
+
+    IEnumerator WaitBeforeTurningOffBulletTime()
+    {
+        yield return new WaitForSeconds(timeUntilTurnOff);
+        bulletTimeMeter.gameObject.SetActive(false);
+    }
+
+
 }
